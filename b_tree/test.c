5393fccd84b46f26e98c24b9f5c6d7852aae03d8
Jacob Schwaiger <jacob.schwaiger@campus.lmu.de>
2021-12-23 12:02:31 +0100

    tested create and print

--- b_tree/test.c
+++ b_tree/test.c
@@ -2,11 +2,18 @@
 #include "b_tree.h"
 
 int main(){
-    TREE t = *(new_b_tree(2, 0));
+    TREE t;
+    create(&t, 2, 0);
 
-    for(int i = 0; i < t.root->keys.capacity; i++){
+    printf("after create: h: %d; d: %d\n", t.height, t.default_key);
+
+    for(int i = 0; i < t.capacity; i++){
          printf("pos %d : %d;",i, t.root->keys.data[0]);
     }
+
+    printf("\n");
+
+    print_b_tree(&t);
 
     destroy(&t);
 
